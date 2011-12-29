e72521c7afbad453c9f17b3e17505abc734257c2
Sean Kermes <seank@softartisans.com>
2011-12-29 13:51:11 -0500

    Adding tester script.

---
+++ test.py
@@ -0,0 +1,93 @@
+#! /usr/bin/python3
+
+import glob
+import os
+import os.path
+import xml.dom
+import xml.dom.minidom
+
+FAILED = 'fail'
+SKIPPED = 'skip'
+
+def childNamed(node, targetName):
+    for kid in node.childNodes:
+        if kid.nodeType == xml.dom.Node.ELEMENT_NODE and kid.localName == targetName:
+            return kid
+    return None
+
+def failure(testNode):
+    return childNamed(testNode, 'failure')
+
+def error(testNode):
+    return childNamed(testNode, 'error')
+
+def skipped(testNode):
+    return childNamed(testNode, 'skipped')
+
+def content(node):
+    return ''.join(text.data for text in node.childNodes if text.nodeType == xml.dom.Node.TEXT_NODE)
+
+def failedTests(reportFolder):
+    prefixLen = len(reportFolder) + 1
+    fixtures = [report[prefixLen:-4] for report in glob.glob(os.path.join(reportFolder, '*.txt'))]
+
+    for fixture in fixtures:
+        report = xml.dom.minidom.parse(os.path.join(reportFolder, 'TEST-' + fixture + '.xml'))
+        for test in report.documentElement.getElementsByTagName('testcase'):
+            fail = failure(test)
+            if fail is not None:
+                yield test.getAttribute('name'), fixture, FAILED, fail.getAttribute('message')
+
+            err = error(test)
+            if err is not None:
+                yield test.getAttribute('name'), fixture, FAILED, '\n'.join(content(err).split('\n')[:2])
+
+            skip = skipped(test)
+            if skip is not None:
+                yield test.getAttribute('name'), fixture, SKIPPED, None
+
+def color(message, color):
+    return '\033[' + color + 'm' + message + '\033[0m'
+
+def red(message):
+    return color(message, '0;31')
+
+def green(message):
+    return color(message, '0;32')
+
+def yellow(message):
+    return color(message, '0;33')
+
+if __name__ == '__main__':
+    os.system('mvn test')
+    print()
+    print()
+
+    lastFixture = ''
+    noFailures = True
+    putNewlineBefore = False
+    for test, fixture, problem, cause in failedTests('target/surefire-reports'):
+        if fixture != lastFixture:
+            print()
+            print(fixture)
+            print('-' * max(80, len(fixture)))
+            lastFixture = fixture
+            putNewlineBefore = False
+
+        if problem == FAILED:
+            if putNewlineBefore:
+                print()
+            print(red(test))
+            print(cause)
+            print()
+            noFailures = False;
+            putNewlineBefore = False
+        elif problem == SKIPPED:
+            print(yellow(test))
+            putNewlineBefore = True
+
+    if noFailures:
+        msg = 'ALL TESTS SUCCESSFUL'
+        print('-' * len(msg))
+        print(green(msg))
+        print('-' * len(msg))
